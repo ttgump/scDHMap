f26ee639eaaa3fcf2c14f35f8ee0a051ad8db5b4
Tian Tian <tiantian@Tians-MacBook-Pro.local>
2022-02-19 16:52:59 -0500

    remove unused code

--- src/scDHMap/preprocess.py
+++ src/scDHMap/preprocess.py
@@ -2,15 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import pickle, os, numbers
-
 import numpy as np
 import scipy as sp
-import pandas as pd
 import scanpy as sc
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import scale
-import sklearn
 
 
 def pearson_residuals(counts, theta, clipping=True):
@@ -31,7 +25,7 @@ def pearson_residuals(counts, theta, clipping=True):
     
     return z
 
-def read_dataset(adata, transpose=False, test_split=False, copy=False):
+def read_dataset(adata, transpose=False, copy=False):
 
     if isinstance(adata, sc.AnnData):
         if copy:
@@ -52,15 +46,6 @@ def read_dataset(adata, transpose=False, test_split=False, copy=False):
 
     if transpose: adata = adata.transpose()
 
-    if test_split:
-        train_idx, test_idx = train_test_split(np.arange(adata.n_obs), test_size=0.1, random_state=42)
-        spl = pd.Series(['train'] * adata.n_obs)
-        spl.iloc[test_idx] = 'test'
-        adata.obs['split'] = spl.values
-    else:
-        adata.obs['split'] = 'train'
-
-    adata.obs['split'] = adata.obs['split'].astype('category')
     print('### Autoencoder: Successfully preprocessed {} genes and {} cells.'.format(adata.n_vars, adata.n_obs))
 
     return adata
@@ -90,23 +75,3 @@ def normalize(adata, filter_min_counts=True, size_factors=True, normalize_input=
         sc.pp.scale(adata)
 
     return adata
-
-def read_genelist(filename):
-    genelist = list(set(open(filename, 'rt').read().strip().split('\n')))
-    assert len(genelist) > 0, 'No genes detected in genelist file'
-    print('### Autoencoder: Subset of {} genes will be denoised.'.format(len(genelist)))
-
-    return genelist
-
-def write_text_matrix(matrix, filename, rownames=None, colnames=None, transpose=False):
-    if transpose:
-        matrix = matrix.T
-        rownames, colnames = colnames, rownames
-
-    pd.DataFrame(matrix, index=rownames, columns=colnames).to_csv(filename,
-                                                                  sep='\t',
-                                                                  index=(rownames is not None),
-                                                                  header=(colnames is not None),
-                                                                  float_format='%.6f')
-def read_pickle(inputfile):
-    return pickle.load(open(inputfile, "rb"))
